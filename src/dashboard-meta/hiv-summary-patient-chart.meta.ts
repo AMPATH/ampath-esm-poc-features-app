d9124fcabaa57f24df07fe16e0237bee44d33166
Felix Maiko <maikofelix89@gmail.com>
2026-05-25 12:49:06 +0300

    (fix) change hiv summary title

--- src/dashboard-meta/hiv-summary-patient-chart.meta.ts
+++ src/dashboard-meta/hiv-summary-patient-chart.meta.ts
@@ -1,6 +1,6 @@
 export const hivSummaryPatientChartMeta = {
   path: 'hiv-summary',
   slot: 'patient-chart-hiv-summary-dashboard-slot',
-  title: 'Hiv Summary',
+  title: 'HIV Summary',
   icon: 'omrs-icon-report',
 };
